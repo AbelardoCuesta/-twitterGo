b1a6bfa76675082b6cd7b18e401721544f2b8be9
AbelardoCuesta <j.cuestaa@uniandes.edu.co>
2023-06-17 16:23:32 -0500

    conexion aws

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import(
 	"os"
 	"strings"
 	"github.com/AbelardoCuesta/twitterGo/awsgo"
+	"github.com/AbelardoCuesta/twitterGo/models"
 )
 func main()  {
 	lambda.Start(EjecutoLambda)
@@ -28,7 +29,30 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest)(*
 		}
 		return res, nil
 	}
-}
+
+	SecretModel, err := secretmanager.GetSecret(os.Getenv("SecretName"))
+	if err != nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Error en la lectura de Secret " + err.Error(),
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil
+	}
+
+	path := strings.Replace(request.PathParameters["twitter"], os.Getenv("UrlPrefix"), "", -1)
+
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("user"), SecretModel.Username)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("password"), SecretModel.Password)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("host"), SecretModel.Host)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("database"), SecretModel.Database)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("jwtSign"), SecretModel.JWTSign)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("body"), request.Body)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("bucketName"), os.Getenv("BucketName"))
 
 func ValidoParametros() bool {
 	_, traeParametro := os.LookupEnv("SecretName")
