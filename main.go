d5a889b2bc732563c71392b3120805dc249df704
AbelardoCuesta <j.cuestaa@uniandes.edu.co>
2023-06-17 14:57:56 -0500

    conexion aws

--- main.go
+++ main.go
@@ -2,11 +2,46 @@ package main
 import(
 	lambda "github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/events"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"github.com/AbelardoCuesta/twitterGo/awsgo"
 )
 func main()  {
 	lambda.Start(EjecutoLambda)
 }
 
 func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest)(*events.APIGatewayProxyResponse, error){
+	var res *events.APIGatewayProxyResponse
 
+	var res *events.APIGatewayProxyResponse
+	awsgo.InicializoAWS()
+
+	if !ValidoParametros() {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Error en las variables de entorno. deben incluir 'SecretName', 'BucketName",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil
+	}
+}
+
+func ValidoParametros() bool {
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro
+	}
+	_, traeParametro = os.LookupEnv("BucketName")
+	if !traeParametro {
+		return traeParametro
+	}
+	_, traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+	}
+	return traeParametro
 }
